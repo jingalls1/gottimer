ac63ee89b019b4da364e54561f4ab80ae429c1b5
Jacob Ingalls <jingalls@uoregon.edu>
2019-04-14 12:03:00 -0700

    finished the code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+import "./styles.css";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {d: new Date()};
   }
-}
 
-export default App;
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({ time: Date.now() }), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render(){
+    let nowD = new Date().getTime();;
+    let gotD = new Date(2019, 4, 14, 18, 0, 0);
+    let t = gotD-nowD;
+    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    let secs = Math.floor((t % (1000 * 60)) / 1000);
+  return (
+    <div className="App">
+      <br style={{lineHeight: "5"}}/>
+      <h3 className="title">alright fuckers, the day has come</h3>
+      <br style={{lineHeight: "3"}}/>
+      <a href="https://fontmeme.com/game-of-thrones-font/"><img src="https://fontmeme.com/permalink/190414/9c428af13e849ebeaf654eac74ddd240.png" alt="game-of-thrones-font" border="0" height="40"/></a>
+      <br/>
+      <a href="https://fontmeme.com/game-of-thrones-font/"><img src="https://fontmeme.com/permalink/190414/63fb69d3a6a03b392c36fc128d308473.png" alt="game-of-thrones-font" border="0" height="35"/></a>
+      <br style={{lineHeight: "5"}}/>
+      <div className="white">
+        <span style={{fontWeight: "bold"}}>{hours}</span> hours &nbsp;
+        <span style={{ fontWeight: "bold" }}>{mins}</span> minutes and &nbsp;
+        <span style={{ fontWeight: "bold" }}>{secs}</span> seconds
+      </div>
+    </div>
+  );
+}}
